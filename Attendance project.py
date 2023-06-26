0f1b8f46dad44da71ce532f9d12b12a377516d10
Ahmed iyanuoluwa <127423641+iyanu752@users.noreply.github.com>
2023-06-26 15:59:28 +0100

    Add files via upload

--- Attendance project.py
+++ Attendance project.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import face_recognition
 import os
+from datetime import datetime
 
 path = 'ImagesAttendance'
 images = []
@@ -23,6 +24,21 @@ def findEncodings(images):
         encodeList.append(encode)
     return encodeList
 
+def markAttendance(name):
+    with open('Attendance.CSV','r+') as f:
+        myDataList = f.readlines()
+        print(myDataList)
+        nameList =[]
+        for line in myDataList:
+            entry = line.split(',')
+            nameList.append(entry[0])
+        if name not in nameList:
+            now = datetime.now()
+            dtstring = now.strftime('%H,%M,%S')
+            f.writelines(f'\n{name},{dtstring}')
+
+
+
 encodeListKnown = findEncodings(images)
 print('Encoding Complete')
 
@@ -39,13 +55,18 @@ while True:
     for encodeFace,faceLoc in zip(encodesCurFrame,facesCurFrame):
          matches = face_recognition.compare_faces(encodeListKnown,encodeFace)
          facedis = face_recognition.face_distance(encodeListKnown, encodeFace)
-         print(facedis)
+        # print(facedis)
          matchIndex = np.argmin(facedis)
 
          if matches[matchIndex]:
              name = classNames[matchIndex].upper()
-             print(name)
-
+            # print(name)
+             y1,x2,y2,x1 = faceLoc
+             y1, x2, y2, x1 = y1*4,x2*4,y2*4,x1*4
+             cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
+             cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0),cv2.FILLED)
+             cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
+             markAttendance(name)
 
     cv2.imshow('Webcam',img)
     cv2.waitKey(1)
